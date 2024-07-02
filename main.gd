280422c1bb8cda74496afba0a0aa44284933b098
aji prasetyo <ajiprxty3@gmail.com>
2024-07-02 20:13:31 +0700

    input action

--- main.gd
+++ main.gd
@@ -6,3 +6,12 @@ func _ready():
 	print("Hello")
 	$Label.text = "Hello Green"
 	$Label.modulate = Color.GREEN
+
+
+func _input(event):
+	if event.is_action_pressed("my_action"):
+		$Label.modulate = Color.RED
+	
+	if event.is_action_released("my_action"):
+		$Label.modulate = Color.GREEN
+	
