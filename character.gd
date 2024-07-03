fff8bf76112e60959d8674c9917161f3dbd5f220
aji prasetyo <ajiprxty3@gmail.com>
2024-07-03 22:47:15 +0700

    inner class

--- character.gd
+++ character.gd
@@ -5,6 +5,20 @@ extends Node
 @export var profession : String
 @export var health : int
 
+var chest = Equipment.new()
+var legs = Equipment.new()
+
+func _ready():
+	chest.armor = 15
+	print(chest.armor)
+	print(legs.weight)
+
+
+class Equipment:
+	var armor := 10
+	var weight := 5
+
 func die():
 	health = 0
 	print(profession + " died.")
+
